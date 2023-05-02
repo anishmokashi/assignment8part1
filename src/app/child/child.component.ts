fbeb65794f05748a0c1a971c91cc6ef603c8e59b
unknown <anish.mokashi13@gmail.com>
2023-05-02 23:32:27 +0530

    seventh commit

---
+++ src/app/child/child.component.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent {
+@Output() public MyEvent=new EventEmitter();
+
+public SendMessage(msg:string)
+  {
+    this.MyEvent.emit(msg);
+  }
+}
